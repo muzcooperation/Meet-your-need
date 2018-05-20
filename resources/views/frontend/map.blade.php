830f7e14d9efb278f8cab1975c8e90a6dc46b48a
Muz <muzcooperation@gmail.com>
2018-05-20 05:28:43 +0500

    dynamically changed zoom level of map to fit all markers

--- resources/views/frontend/map.blade.php
+++ resources/views/frontend/map.blade.php
@@ -9,6 +9,7 @@
     var currLat;
     var currLong;
     var markers = [];
+    var bounds;
 
     function showPosition(position) {
       currLat = position.coords.latitude;
@@ -18,6 +19,7 @@
           center: {lat: currLat, lng: currLong},
           zoom: 20
       });
+
     }
   
     function initMap() {
@@ -29,6 +31,11 @@
     }
 
     function searchNearByLocations() {
+      console.log(markers);
+      if (markers.length) {
+        deleteMarkers();
+      } 
+
       var placeType = $('#places').find(":selected").text();
       placeType = placeType.toLowerCase();
       
@@ -46,22 +53,6 @@
             type: [placeType]
           }, callback);
 
-          // var bounds = new google.maps.LatLngBounds();
-
-
-          // for (let i = 0; i < markers.length; i++) {
-          //   const marker = markers[i];
-          //   const newPoint = new google.maps.LatLng(marker.lat, marker.lng);
-          //   bounds.extend(newPoint);
-          // }
-
-          // map.fitBounds(bounds);
-
-          // bounds.extend(marker.getPosition());
-          // map.fitBounds(bounds);
-
-          //remove one zoom level to ensure no marker is on the edge.
-          //map.setZoom(map.getZoom()-1);
         } else {
           alert('please enter radius');
         }
@@ -72,34 +63,57 @@
 
     function callback(results, status) {
       console.log(results);
+      if (results.length <= 0) {
+        alert('No location found');
+        return;
+      }
+
       if (status === google.maps.places.PlacesServiceStatus.OK) {
+        bounds = new google.maps.LatLngBounds();
+
         for (var i = 0; i < results.length; i++) {
           createMarker(results[i]);
         }
+
+        map.fitBounds(bounds);
+        //remove one zoom level to ensure no marker is on the edge.
+        map.setZoom(map.getZoom()-1); 
       }
     }
 
     function createMarker(place) {
       var placeLoc = place.geometry.location;
-      var bounds = new google.maps.LatLngBounds();
       var marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location
       });
 
-      // markers.push(marker);
-      map.fitBounds(bounds);
+      markers.push(marker);
 
       bounds.extend(marker.getPosition());
-      map.fitBounds(bounds);
-
-      // remove one zoom level to ensure no marker is on the edge.
-      map.setZoom(map.getZoom()-1);
-
+    
       google.maps.event.addListener(marker, 'click', function() {
         infowindow.setContent(place.name);
         infowindow.open(map, this);
       });
+    }
+
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+      setMapOnAll(null);
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+      clearMarkers();
+      markers = [];
     }
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key={{config('constants.google_api_key')}}&&libraries=places&callback=initMap"
