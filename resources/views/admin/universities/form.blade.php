4a409e6aae4f3db20dea3f712ececcdc4566c3b8
Muz <muzcooperation@gmail.com>
2018-06-03 16:43:41 +0500

    changed search box with auto complete

--- resources/views/admin/universities/form.blade.php
+++ resources/views/admin/universities/form.blade.php
@@ -128,55 +128,65 @@
 
           // Create the search box and link it to the UI element.
           var input = $('#map-search-input')[0];
-          var searchBox = new google.maps.places.SearchBox(input);
           map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-          // Bias the SearchBox results towards current map's viewport.
-          map.addListener('bounds_changed', function() {
-            searchBox.setBounds(map.getBounds());
-          });
-
           var markers = [];
-          // Listen for the event fired when the user selects a prediction and retrieve
-          // more details for that place.
-          searchBox.addListener('places_changed', function() {
-              var places = searchBox.getPlaces();
+          var autocomplete = new google.maps.places.Autocomplete(input);
 
-              if (places.length == 0) {
-                  return;
-              }
+          // Bind the map's bounds (viewport) property to the autocomplete object,
+          // so that the autocomplete requests use the current map bounds for the
+          // bounds option in the request.
+          autocomplete.bindTo('bounds', map);
+          var markers = [];
 
-                // Clear out the old markers.
-              markers.forEach(function(marker) {
-                  marker.setMap(null);
-              });
-              markers = [];
+          autocomplete.addListener('place_changed', function() {
+            var place = autocomplete.getPlace();
 
-                // For each place, get the icon, name and location.
-              var bounds = new google.maps.LatLngBounds();
-              places.forEach(function(place) {
-                  console.log(place, 'search result');
+            if (!place.geometry) {
+              // User entered the name of a Place that was not suggested and
+              // pressed the Enter key, or the Place Details request failed.
+              window.alert("No details available for input: '" + place.name + "'");
+              return;
+            }
 
-                  if (!place.geometry) {
-                    console.log("Returned place contains no geometry");
-                    return;
-                  }
-                  // Create a marker for each place.
-                  markers.push(new google.maps.Marker({
-                    map: map,
-                    title: place.name,
-                    position: place.geometry.location
-                  }));
-
-                  if (place.geometry.viewport) {
-                    // Only geocodes have viewport.
-                    bounds.union(place.geometry.viewport);
-                  } else {
-                    bounds.extend(place.geometry.location);
-                  }
-              });
-              map.fitBounds(bounds);
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+              map.fitBounds(place.geometry.viewport);
+            } else {
+              map.setCenter(place.geometry.location);
+              map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            
+            markers.forEach(function(marker) {
+                marker.setMap(null);
             });
+            markers = [];
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+
+            console.log(place, 'search result');
+
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+           
+            map.fitBounds(bounds);
+          });
         }
 
         function initAutocomplete() {
